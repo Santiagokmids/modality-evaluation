0297bf5b9617da61d30c9d956ab7c04135ad7d55
SANTIAGO TROCHEZ VELASCO <1193573597@u.icesi.edu.co>
2022-05-01 15:40:49 -0500

    Questions and answers were added

--- modality_evaluation/src/pages/api/teacher/questions/[searchQuestion].js
+++ modality_evaluation/src/pages/api/teacher/questions/[searchQuestion].js
@@ -2,11 +2,34 @@ import database from "../../../../util/questions";
 
 export default async function searchQuestion(req, res) {
 
+    const {body} = req;
+
     try {
         let response = await database.query("SELECT * FROM QUESTIONS WHERE ID = $1", [req.query.searchQuestion]);
 
         if (response.rows.length = 1) {
-            res.status(200).json({ date: response.rows[0] });
+
+            switch (body.num) {
+                case "1":
+                    res.status(200).json({ question: response.rows[0].question1, answer1: response.rows[0].answera1, answer2: response.rows[0].answerb1, answer3: response.rows[0].answerc1, answer4: response.rows[0].answerd1 });
+                    break;
+
+                case "2":
+                    res.status(200).json({ question: response.rows[0].question2, answer1: response.rows[0].answera2, answer2: response.rows[0].answerb2, answer3: response.rows[0].answerc2, answer4: response.rows[0].answerd2 });
+                    break;
+
+                case "3":
+                    res.status(200).json({ question: response.rows[0].question3, answer1: response.rows[0].answera3, answer2: response.rows[0].answerb3, answer3: response.rows[0].answerc3, answer4: response.rows[0].answerd3 });
+                    break;
+
+                case "4":
+                    res.status(200).json({ question: response.rows[0].question4, answer1: response.rows[0].answera4, answer2: response.rows[0].answerb4, answer3: response.rows[0].answerc4, answer4: response.rows[0].answerd4 });
+                    break;
+
+                case "5":
+                    res.status(200).json({ question: response.rows[0].question5, answer1: response.rows[0].answera5, answer2: response.rows[0].answerb5, answer3: response.rows[0].answerc5, answer4: response.rows[0].answerd5 });
+                    break;
+            }
 
         } else {
             res.status(400).json({ date: "not found" });
--- modality_evaluation/src/pages/front/[doingTest].js
+++ modality_evaluation/src/pages/front/[doingTest].js
@@ -1,6 +1,9 @@
 import Header from '../../components/Header';
 import styles from '../../styles/Home.module.css';
 
+let router = "";
+let number = "1";
+
 export default function test({ title }) {
 
     return (
@@ -9,12 +12,12 @@ export default function test({ title }) {
             <div className={styles.mainContainer}>
                 <div className="card" style={{ width: "1000px", height: "500px" }}>
                     <div className="card-body">
-                        <h5 className="card-title"> {title.question1} </h5>
+                        <h5 className="card-title"> {title.question} </h5>
                         <p className="card-text"> Seleccione una opción </p>
-                        <input type="radio" name="answer1" value="A" /> {title.answera1} <br />
-                        <input type="radio" name="answer1" value="B" /> {title.answerb1} <br />
-                        <input type="radio" name="answer1" value="C" /> {title.answerc1} <br />
-                        <input type="radio" name="answer1" value="D" /> {title.answerd1} <br />
+                        <input type="radio" name="answer1" value="A" /> {title.answer1} <br />
+                        <input type="radio" name="answer1" value="B" /> {title.answer2} <br />
+                        <input type="radio" name="answer1" value="C" /> {title.answer3} <br />
+                        <input type="radio" name="answer1" value="D" /> {title.answer4} <br />
                     </div>
                 </div>
             </div>
@@ -26,11 +29,31 @@ export default function test({ title }) {
                                 <span aria-hidden="true">&laquo;</span>
                             </a>
                         </li>
-                        <li className="page-item"><a className="page-link" href="1">1</a></li>
-                        <li className="page-item"><a className="page-link" href="2">2</a></li>
-                        <li className="page-item"><a className="page-link" href="3">3</a></li>
-                        <li className="page-item"><a className="page-link" href="4">4</a></li>
-                        <li className="page-item"><a className="page-link" href="5">5</a></li>
+                        <li className="page-item">
+                            <a className="page-link" href={router + "-1"}>
+                                <button type='submit' className='page-link' style={{ background: "none", color: "inherit", border: "none" }} > 1 </button>
+                            </a>
+                        </li>
+                        <li className="page-item">
+                            <a className="page-link" href={router + "-2"}>
+                                <button type='submit' className='page-link' style={{ background: "none", color: "inherit", border: "none" }} > 2 </button>
+                            </a>
+                        </li>
+                        <li className="page-item">
+                            <a className="page-link" href={router + "-3"}>
+                                <button type='submit' className='page-link' style={{ background: "none", color: "inherit", border: "none" }} > 3 </button>
+                            </a>
+                        </li>
+                        <li className="page-item">
+                            <a className="page-link" href={router + "-4"}>
+                                <button type='submit' className='page-link' style={{ background: "none", color: "inherit", border: "none" }} > 4 </button>
+                            </a>
+                        </li>
+                        <li className="page-item">
+                            <a className="page-link" href={router + "-5"}>
+                                <button type='submit' className='page-link' style={{ background: "none", color: "inherit", border: "none" }} > 5 </button>
+                            </a>
+                        </li>
                         <li className="page-item">
                             <a className="page-link" href="#" aria-label="Next">
                                 <span aria-hidden="true">&raquo;</span>
@@ -48,13 +71,45 @@ test.getInitialProps = async ({ asPath }) => {
     let text = "";
     let rute = "";
     let title = "";
+    let verify = false;
+    let r = "";
 
     text = asPath;
     rute = text.split("/");
 
-    let r = await fetch("http://localhost:3000/api/teacher/questions/" + rute[2]);
-    const question = await r.json();
-    title = question.date;
+    for (let i = 0; i < rute.length; i++) {
+
+        if (rute[i].indexOf("-") > -1) {
+            text = rute[i].split("-");
+            verify = true;
+            number = text[1];
+        }
+    }
+
+    let state = {
+        num: number,
+    };
+
+    let object = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(state)
+    }
+
+    if (!verify) {
+        r = await fetch("http://localhost:3000/api/teacher/questions/" + rute[2], object);
+        router = "http://localhost:3000/front/" + rute[2];
+
+    } else {
+        r = await fetch("http://localhost:3000/api/teacher/questions/" + text[0], object);
+        router = "http://localhost:3000/front/" + text[0];
+    }
+
+    const questions = await r.json();
+    title = questions;
 
     return { title };
 };
