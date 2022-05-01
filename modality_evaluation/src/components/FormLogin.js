4d5dc8a43e6e576bc8f02b0dde8b70ba68a558ec
Luis Miguel Ossa Arias <1006016025@u.icesi.edu.co>
2022-05-01 15:39:18 -0500

    The style of the login and register has been changed

---
+++ modality_evaluation/src/components/FormLogin.js
@@ -0,0 +1,85 @@
+import { Component } from 'react'
+import styles from "../styles/Home.module.css";
+
+export default class Form extends Component {
+
+    state = {
+        name: "",
+        password: "",
+        type: 0
+    }
+
+    handleChange = e => {
+        this.setState({
+            ...this.state.form,
+            [e.target.name]: e.target.value
+        })
+        console.log(this.state);
+    }
+
+    handleSubmit = async e => {
+
+        e.preventDefault();
+        let object = {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(this.state)
+        }
+
+        let r = await fetch("http://localhost:3000/api/student/loginInPage", object);
+
+        const result = await r.json();
+
+        if (result.type == 1) {
+            window.location.href = "http://localhost:3000/front/createTest"
+
+        } else if (result.type == 2) {
+            window.location.href = "http://localhost:3000/front/test"
+
+        } else {
+            window.alert("No se encuentra registrado en la base de datos. Registrese!");
+        }
+    }
+
+    render() {
+        return (
+            <div className={styles.mainContainer}>
+                <div className="card" style={{width: "30rem", height: "25rem", marginTop: "132px"}}>
+                    <div className="card-body">
+                        <h5 className="card-title">Iniciar sesión</h5>
+                        <form onSubmit={this.handleSubmit} onChange={this.handleChange}>
+                            <div className="mb-3">
+                                <label htmlFor="exampleInputEmail1" className="form-label">Nombre completo</label>
+                                <input name="name" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required />
+                                <div id="emailHelp" className="form-text">Nunca compartiremos tu información con alguien más.</div>
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="exampleInputPassword1" className="form-label">Contraseña</label>
+                                <input name="password" type="password" className="form-control" id="exampleInputPassword1" required />
+                            </div>
+                            <label htmlFor="exampleInputPassword1" className="form-label">Yo soy un...</label>
+                            <br></br>
+                            <div className="btn-group" role="group" aria-label="Basic radio toggle button group">
+                                <input type="radio" className="btn-check" name="profesion" id="btnradio1" defaultValue={1} autoComplete="off" />
+                                <label className="btn btn-outline-primary" htmlFor="btnradio1">Profesor</label>
+                                <input type="radio" className="btn-check" name="profesion" id="btnradio3" defaultValue={2} autoComplete="off" />
+                                <label className="btn btn-outline-primary" htmlFor="btnradio3">Estudiante</label>
+                            </div>
+
+                            <br></br>
+                            <br></br>
+
+                            <button type="submit" className="btn btn-primary" style={{ marginRight: "50px" }}>Ingresar</button>
+                        </form>
+                        <a href="http://localhost:3000/front/register">
+                            <button className="btn btn-primary" style={{ marginTop: "-65px", marginLeft: "108px" }}>Registrarse</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
--- modality_evaluation/src/pages/index.js
+++ modality_evaluation/src/pages/index.js
@@ -1,86 +1,11 @@
 import Header from "../components/Header";
-import styles from "../styles/Home.module.css";
-
-let state = {
-    name: "",
-    password: "",
-    type: 0
-}
-
-let handleChange = e => {
-
-    switch (e.target.name) {
-        case "name":
-            state.name = e.target.value
-            break;
-
-        case "password":
-            state.password = e.target.value
-            break;
-
-        case "profesion":
-            state.type = e.target.value
-            break;
-    }
-}
-
-let handleSubmit = async e => {
-
-    e.preventDefault();
-    let object = {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(state)
-    }
-
-    let r = await fetch("http://localhost:3000/api/student/loginInPage", object);
-
-    const result = await r.json();
-
-    if (result.type == 1) {
-        window.location.href = "http://localhost:3000/front/createTest"
-
-    } else if (result.type == 2) {
-        window.location.href = "http://localhost:3000/front/test"
-        
-    } else {
-        window.alert("No se encuentra registrado en la base de datos. Registrese!");
-    }
-}
+import FormLogin from "../components/FormLogin";
 
 export default function loginRegister() {
     return (
         <div>
-            <Header title="Login"> </Header>
-            <h1 className={styles.hola}> Ingresa o registrate al módulo de exámenes </h1>
-            <card className={styles.cardd} onSubmit={handleSubmit}>
-                <form className="form">
-                    <div className="form-group p-2" onChange={handleChange}>
-                        <input className="form-control" type="text" placeholder="Nombre completo" name="name" required />
-                    </div>
-
-                    <div className="form-group p-2" onChange={handleChange}>
-                        <input className="form-control" type="password" placeholder="Contraseña" name="password" required />
-                    </div>
-
-                    <div className="form-group p-2" onChange={handleChange}>
-                        <input type="radio" name="profesion" value="1" /> Profesor <input type="radio" name="profesion" value="2" />Estudiante <br />
-                    </div>
-
-                    <div className="form-group p-2">
-                        <button id="login" type="submit" > Ingresar </button>
-                    </div>
-
-                </form>
-            </card>
-            <div style={{ textAlign: "center" }}>
-                <a href="http://localhost:3000/front/register">
-                    <button>Registrarse</button>
-                </a>
-            </div>
+            <Header title="logIn"></Header>
+            <FormLogin></FormLogin>
         </div>
     )
 }
